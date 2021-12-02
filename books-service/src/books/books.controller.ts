7723372a6c3798d58782727b35580a708fbbefbd
nilay <nilay.inexture@gmail.com>
2021-12-02 14:10:19 +0530

    init

--- books-service/src/books/books.controller.ts
+++ books-service/src/books/books.controller.ts
@@ -1,41 +1,41 @@
 import { BooksService } from "./books.service";
 import {
-  Controller,
-  Get,
-  Param,
-  Post,
-  Delete,
-  Query,
-  Body,
+    Controller,
+    Get,
+    Param,
+    Post,
+    Delete,
+    Query,
+    Body,
 } from "@nestjs/common";
 import { CreateBookDTO } from "./create-book.dto";
 import { MessagePattern } from "@nestjs/microservices";
 
 @Controller("books")
 export class BooksController {
-  constructor(private booksService: BooksService) {}
+    constructor(private booksService: BooksService) {}
 
-  @MessagePattern({ cmd: "getBooks" })
-  async getBooks() {
-    const books = await this.booksService.getBooks();
-    return books;
-  }
+    @MessagePattern({ cmd: "getBooks" })
+    async getBooks() {
+        const books = await this.booksService.getBooks();
+        return books;
+    }
 
-  @MessagePattern({ cmd: "getBookById" })
-  async getBook(id) {
-    const book = await this.booksService.getBook(id);
-    return book;
-  }
+    @MessagePattern({ cmd: "getBookById" })
+    async getBook(id) {
+        const book = await this.booksService.getBook(id);
+        return book;
+    }
 
-  @MessagePattern({ cmd: "addBook" })
-  async addBook(createBookDTO: CreateBookDTO) {
-    const book = await this.booksService.addBook(createBookDTO);
-    return book;
-  }
+    @MessagePattern({ cmd: "addBook" })
+    async addBook(createBookDTO: CreateBookDTO) {
+        const book = await this.booksService.addBook(createBookDTO);
+        return book;
+    }
 
-  @MessagePattern({ cmd: "deleteBook" })
-  async deleteBook(id) {
-    const books = await this.booksService.deleteBook(id);
-    return books;
-  }
+    @MessagePattern({ cmd: "deleteBook" })
+    async deleteBook(id) {
+        const books = await this.booksService.deleteBook(id);
+        return books;
+    }
 }
